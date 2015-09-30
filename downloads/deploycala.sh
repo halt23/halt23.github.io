88a4fa00def1bdb441bb33640e00cb38f8245849
Teo Mrnjavac <teo@kde.org>
2015-09-30 15:50:25 +0200

    Submodule.

--- downloads/deploycala.sh
+++ downloads/deploycala.sh
@@ -5,6 +5,8 @@ yaourt -Sy --noconfirm cmake extra-cmake-modules boost
 git clone https://github.com/calamares/calamares.git
 cd calamares
 git checkout --track origin/1.1.x-stable -b 1.1.x-stable
+git submodule init
+git submodule update
 mkdir build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/usr -DWITH_PARTITIONMANAGER=1 ..
