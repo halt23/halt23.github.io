15826408837cb9292f9d855c72c8473cee3f1c57
Teo Mrnjavac <teo@kde.org>
2015-11-12 13:18:23 +0100

    Make the deploy script work more than once.

--- deploycala.sh
+++ deploycala.sh
@@ -12,11 +12,23 @@ if [ -z "$1" ]; then
     BRANCH=master
 fi
 
-git clone https://github.com/calamares/calamares.git
-cd calamares
-git checkout --track origin/$BRANCH -b $BRANCH
-git submodule init
-git submodule update
+if [ -d calamares ]
+    cd calamares
+    git checkout --track origin/$BRANCH -b $BRANCH
+    git pull --rebase
+    git submodule update
+    git submodule sync
+    if [ -d build ]
+        rm -rf build
+    fi
+else
+    git clone https://github.com/calamares/calamares.git
+    cd calamares
+    git checkout --track origin/$BRANCH -b $BRANCH
+    git submodule init
+    git submodule update
+    git submodule sync
+fi
 mkdir build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/usr -DWITH_PARTITIONMANAGER=1 ..
