33d9f9f29a086a6aa2c74ccc910ee141f97cc434
shainer <syn.shainer@gmail.com>
2016-07-15 17:46:50 +0100

    Set default value or --url flag using argparse itself.

--- deploycala.py
+++ deploycala.py
@@ -206,8 +206,8 @@ def main():
     parser.add_argument("-i", "--incremental", action="store_true", dest="incremental",
                         help="do incremental builds, i.e. don't clear the build directory before building, if found")
     parser.add_argument("--noupdate", action="store_true", dest="noupdate", help=argparse.SUPPRESS)
-    parser.add_argument("-u", "--url", nargs=1, default=None, dest="url",
-                        help="change the remote URL we clone Calamares from.")
+    parser.add_argument("-u", "--url", nargs=1, default="https://github.com/calamares/calamares.git",
+                        dest="url", help="change the remote URL we clone Calamares from.")
     args = parser.parse_args()
 
     if not args.noupdate:
@@ -275,13 +275,8 @@ def main():
         if args.nopull:
             bail("existing clone not found, can't build without pulling.")
 
-        if args.url:
-            url = args.url[0]
-        else:
-            url = "https://github.com/calamares/calamares.git"
-
         message("Cloning and checking out " + branch + " branch...")
-        os.system("git clone %s" % url)
+        os.system("git clone %s" % args.url)
         os.chdir("calamares")
         os.system("git checkout --track origin/"+ branch +" -b " + branch)
         os.system("git submodule init")
